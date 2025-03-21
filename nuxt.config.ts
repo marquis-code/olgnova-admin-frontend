6d28f931e502998506ccffc7f595f560dc9aa1d0
Kelvin Edosa <edosakelvin@gmail.com>
2025-03-21 13:43:24 +0100

    refactor nuxt.config.ts for improved readability and remove unused .htaccess file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,10 +19,13 @@ export default defineNuxtConfig({
         { name: "viewport", content: "width=device-width, initial-scale=1" },
         { name: "format-detection", content: "telephone=no" },
       ],
-      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.svg" },    {
-        rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css2?family=Lexend+Tera:wght@100..900&family=Rethink+Sans:ital,wght@0,400..800;1,400..800&display=swap',
-      }],
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "/favicon.svg" },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Lexend+Tera:wght@100..900&family=Rethink+Sans:ital,wght@0,400..800;1,400..800&display=swap",
+        },
+      ],
       script: [
         {
           src: "https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js",
@@ -34,27 +37,30 @@ export default defineNuxtConfig({
 
   googleFonts: {
     families: {
-      'Lexend+Tera': [400, 500, 700], // Specify font weights as needed
+      "Lexend+Tera": [400, 500, 700], // Specify font weights as needed
     },
-    display: 'swap', // Optional: Controls how the font is displayed
+    display: "swap", // Optional: Controls how the font is displayed
     prefetch: true, // Optional: Prefetch the font for faster loading
     preconnect: true, // Optional: Preconnect to the font server
     preload: true, // Optional: Preload the font
   },
   css: ["/assets/css/main.css"],
-  modules: ['@kevinmarrec/nuxt-pwa', "@nuxtjs/tailwindcss", '@nuxtjs/google-fonts'],
+  modules: [
+    "@kevinmarrec/nuxt-pwa",
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/google-fonts",
+  ],
   pwa: {
     workbox: {
-      enabled: true
+      enabled: true,
     },
     meta: {
       title: "Withcapera Website",
       author: "Marquis",
       mobileAppIOS: false,
       mobileApp: true,
-      description:
-        "Withcapera Website",
-      theme_color: "#27396B"
+      description: "Withcapera Website",
+      theme_color: "#27396B",
     },
     icon: {
       sizes: [64, 120, 144, 152, 192, 384, 512],
@@ -62,6 +68,6 @@ export default defineNuxtConfig({
     manifest: {
       name: "Marquis",
       lang: "fa",
-    }
-  }
+    },
+  },
 });
