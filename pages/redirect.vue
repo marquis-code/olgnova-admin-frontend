f3ae8538c33e39ab3642b9ab84476da54f6816bc
Marquis <abahmarquis@gmail.com>
2025-04-22 12:31:55 +0100

    deployed updated favicon

---
+++ pages/redirect.vue
@@ -0,0 +1,13 @@
+<template>
+    <button @click="redirectToEmail">Click to Email Me</button>
+  </template>
+  
+  <script>
+  export default {
+    methods: {
+      redirectToEmail() {
+        window.location.href = "mailto:your-email@example.com";
+      },
+    },
+  };
+  </script>
